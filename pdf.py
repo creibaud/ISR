3d3a20c553e12c950206787f40b616457be2edbe
creibaud <creibaud@guardiaschool.fr>
2025-04-26 17:40:14 +0200

    Update cheese data scraping script and add cheese list CSV
    - Changed the target URL in pdf.py to scrape cheese data from a new source.
    - Modified the scraping logic to extract cheese names and descriptions from a table.
    - Implemented CSV writing functionality to save the scraped cheese data into fromages.csv.
    - Added a comprehensive list of cheeses to fromages.csv.

--- pdf.py
+++ pdf.py
@@ -1,32 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-# URL cible
-url = "https://www.generationpeche.fr/181-les-fiches-poissons.htm"
+# URL de la page
+url = "https://www.laboitedufromager.com/liste-des-fromages-par-ordre-alphabetique/"
 
-# Faire la requête HTTP
+# Faire la requête
 response = requests.get(url)
 
 result = []
 
-# Vérifier que la requête s'est bien passée
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Trouver la div avec l'id 'int-plante-tab'
-    int_plante_tabs = soup.find_all("ul", class_="item")
+    # Récupérer tous les <td>
+    table = soup.find("table")
+    if table:
+        rows = table.find_all("tr")
+        for row in rows:
+            cols = row.find_all("td")
+            cols = [col.text.strip() for col in cols]
+            if cols:
+                result.append(cols)
 
-    if int_plante_tabs:
-        for tab in int_plante_tabs:
-            # Récupérer tous les h3 à l'intérieur de chaque div
-            h3_elements = tab.find_all("h2")
-
-            # Afficher les textes
-            for h3 in h3_elements:
-                text = h3.get_text(strip=True)
-                result.append(text.upper())
-    else:
-        print("Aucune div avec la classe 'int-plante-tab' trouvée sur la page.")
+        # Écrire les résultats dans un fichier CSV
+        with open("fromages.csv", "w", newline="", encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(["Nom", "Description"])  # En-têtes du fichier CSV
+            writer.writerows(result)
 else:
     print(
         f"Erreur lors de la récupération de la page. Status code: {response.status_code}"
